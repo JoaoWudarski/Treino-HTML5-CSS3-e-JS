01278eac416c3f4892481d569c393e799f827448
JoaoWudarski <joaowudarski@gmail.com>
2021-01-27 12:35:09 -0300

    primeiroCommit

---
+++ mainjavascript.js
@@ -0,0 +1,72 @@
+function botao(){
+    document.getElementById("agradecimento").innerHTML = "çjaflsdfsadfnsdflknsdfaion fiouvbharpuiahpsdfuiasdhfaisufsdhfisdhfaas ajsdfoiasdjfaiosdfjhsadofihasdfosahdfoisadhfd sdfoiha sdofisahdfosaidfha sdfoashdfoasdfh saduiofhsdfuihasdf iuasdfhsau idfhasdifugadfdsapfaghsdf asdf asghpdfhsad fpsioadh fsadiofhasdfoihs dfdhpfsduipafghsdfiu asdghfpsdgf";
+}
+
+function intz_pass(){
+    exibir();
+    document.getElementById("x").innerHTML = "A INTZ entrou nos e-Sports em junho de 2014. Desde então, com 1 título em 2015, 2 em 2016, 1 em 2019 e seu último em 2020, a equipe intrépida é pentacampeã do CBLOL."
+}
+
+function pain_pass(){
+    exibir();
+    document.getElementById("x").innerHTML = "A PAIN Gamning é uma das equipes mais antigas do cenário, surgindo no primeiro CBLOL na BGS DE 2012. Era também, quem possuia uma das maiores torcidas, até então... A Pain é bicampeã do CBLOL, sendo os títulos conquistados em 2013 e 2015."
+}
+
+function fla_pass(){
+    exibir2();
+    document.getElementById("y").innerHTML = "O Flamengo eSports, gigante do futebol, entrou para o cenário em 2018, indo do antigo circuito desafiante, até a final do CBLOL naquele ano. A equipe, sempre com grande investimento, bateu na trave mais de uma vez, mas conseguiu conquistar seu título em setembro de 2019."
+}
+
+function exibir(){
+    document.getElementById("x").style.display = "block";
+}
+
+function ocultar(){
+    document.getElementById("x").style.display = "none";
+}
+
+function exibir2(){
+    document.getElementById("y").style.display = "block";
+}
+
+function ocultar2(){
+    document.getElementById("y").style.display = "none";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function redirect(){
+    //window.open("https://www.google.com");
+    window.location.href = "https://www.google.com";
+}
+
+function trade(x){
+    x.innerHTML = "JONNYCANABRABA";
+    //document.getElementById(x).innerHTML = "JONNYCANABRABA";
+}
+
+function back(x){
+    x.innerHTML = "joaowudarski@gmail.com";
+    //document.getElementById(x).innerHTML = "joaowudarski@gmail.com";
+}
+
